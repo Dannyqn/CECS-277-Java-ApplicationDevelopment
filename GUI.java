3a7164b1c628bb2eeadf5d7691081f4af5a5a60c
Kenny Nguyen <ndanny222@gmail.com>
2018-09-25 13:03:54 -0700

    added Adder.java and GUI.java

---
+++ GUI.java
@@ -0,0 +1,12 @@
+package GUI.main;
+
+import javax.swing.JFrame;
+
+public class GUI {
+	
+	public static void main(String[] args){
+		JFrame frame = new Adder();
+		frame.setVisible(true);
+	}
+	
+}
